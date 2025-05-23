470ad4fca2e4f0dcedd92fad4d78604a77f9bd77
AhrorovK <akhrorovk5758@gmail.com>
2025-05-23 22:43:03 +0300

    update readme

--- app/src/main/java/com/example/newsaggregator/core/Helper.kt
+++ app/src/main/java/com/example/newsaggregator/core/Helper.kt
@@ -4,7 +4,6 @@ import com.example.newsaggregator.data.rss.dto.ItemDto
 
 fun searchFilter(item: ItemDto, searchState: String): Boolean {
     return item.title.toLowerCase().contains(searchState.toLowerCase()) ||
-            item.description.toLowerCase()
-                .contains(searchState.toLowerCase()) ||
+            item.description.toLowerCase().contains(searchState.toLowerCase()) ||
             item.dcCreator.toLowerCase().contains(searchState.toLowerCase())
 }
